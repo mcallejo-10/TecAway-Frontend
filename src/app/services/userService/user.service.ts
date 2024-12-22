d7b778f87b4d81bd8609196d5ae504d9492ff04f
Miranda <mirandacallejonh@gmail.com>
2024-12-22 22:12:04 +0100

    user service updated

--- src/app/services/userService/user.service.ts
+++ src/app/services/userService/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { User } from '../../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,22 @@ export class UserService {
     this.myAppUrl = environment.endpoint;
     this.myApiUrl = '/api/user/';
   }
-  // getUserList() {
-  // }
+  getUserList(): Observable<User[]> {
+    return this.http.get<User[]>(this.myAppUrl + this.myApiUrl + 'get-all-users');  
+  }
+
+  getUserById(): Observable<User> {
+    return this.http.get<User>(this.myAppUrl + this.myApiUrl);
+  }
+
+  updateUser(user: User): Observable<any> {
+    return this.http.patch(this.myAppUrl + this.myApiUrl, user);
+    
+  }
+
+  uploadPhoto(photo: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', photo);
+    return this.http.post<any>(this.myAppUrl + this.myApiUrl + 'upload-photo', formData);
+  }
 }
