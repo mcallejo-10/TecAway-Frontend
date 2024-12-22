b3b64000d76873de7ea77c4e0f6b7b6faa12d9dd
Miranda <mirandacallejonh@gmail.com>
2024-12-22 23:32:18 +0100

    auth service updated

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { User } from '../../interfaces/user';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private myAppUrl: string;
+  private myApiUrl: string;
+  
+  constructor(private http: HttpClient) {  
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = '/api/auth/';}
 
-  constructor() { }
+    registerUser(user: User): Observable<any> {
+      return this.http.post(this.myAppUrl + this.myApiUrl + 'register', user);
+    }
+
+    loginUser(user: User): Observable<any> {
+      return this.http.post(this.myAppUrl + this.myApiUrl + 'login', user);
+    }
+
+    forgotPassword(email: string): Observable<any> {
+      return this.http.post(this.myAppUrl + this.myApiUrl + 'forgot-password', email);
+    }
+
+    changePassword(user: User): Observable<any> {
+      return this.http.post(this.myAppUrl + this.myApiUrl + 'change-password', user);
+    }
+
+    logoutUser(): Observable<any> {
+      return this.http.get(this.myAppUrl + this.myApiUrl + 'logout');
+    }
 }
+
+
